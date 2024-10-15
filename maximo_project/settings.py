dd27f0d0ddcb1a6356fe5eaa014a36fe5e606732
naravid19 <57556702+naravid19@users.noreply.github.com>
2024-10-15 20:20:35 +0700

    Add port

--- maximo_project/settings.py
+++ maximo_project/settings.py
@@ -18,7 +18,7 @@ from concurrent_log_handler import ConcurrentRotatingFileHandler
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+PORT = int(os.environ.get("PORT", 8000))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
